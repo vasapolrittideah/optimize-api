de50a902bf5077921b8380c6db9f5b317b8c5860
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2025-09-11 14:08:10 +0700

    feat(core): add utility for gRPC health check service registration

---
+++ shared/utilities/grpc.go
@@ -0,0 +1,14 @@
+package utilities
+
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
+)
+
+// RegisterHealthServer registers the gRPC health check service.
+func RegisterHealthServer(grpcServer *grpc.Server) {
+	healthServer := health.NewServer()
+	healthServer.SetServingStatus("", grpc_health_v1.HealthCheckResponse_SERVING)
+	grpc_health_v1.RegisterHealthServer(grpcServer, healthServer)
+}
